eacfcc37081f79421bc8e3e91b6947ebea139ffb
1zhur1 <sen99943@gmail.com>
2026-05-21 17:24:05 +0300

    modified:   config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-import os
-
-# Если переменная окружения не задана — используется значение по умолчанию
-BOT_TOKEN = os.getenv("BOT_TOKEN") or "8844414313:AAEj5XZ6g_URkRiF-vuNUQY6WoZG3HecXJA"
+BOT_TOKEN = "8844414313:AAEj5XZ6g_URkRiF-vuNUQY6WoZG3HecXJA"
 
 CHANNEL_USERNAME = "@WTF_steam"
